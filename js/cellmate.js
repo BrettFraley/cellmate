02e2b53435e787c7be67f0b38bb971be46abd1a9
BrettFraley <brettfraley@proton.me>
2024-01-22 03:16:02 -0500

    Adds color picker form and functionality

--- js/cellmate.js
+++ js/cellmate.js
@@ -14,12 +14,14 @@ const cellmate = {
 
 const generator = () => {
     
-    // Clear / Rest canvas
-    // TODO: make "if rest true setting"
+    // Clear / Reset canvas
+    // TODO: make "if reset true setting"
     ctx.fillStyle = '#fff'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    let colors = ['_', 'red', 'green', 'blue']
+    let chosenColors = getChosenColors()
+    let colors = chosenColors.length > 0 ? chosenColors : ['_', 'red', 'green', 'blue']
+
     let numShapes = parseInt(document.getElementById('generator-num-of-shapes').value)
 
     // Width and height
@@ -42,9 +44,40 @@ const generator = () => {
     }
 }
 
-// UI event listeners
+/**  UI event listeners  */
+
+// Main generate UI action
 let generateButton = document.getElementById('generate-button')
 
 generateButton.addEventListener('click', () => {
     generator()
 }, false)
+
+// Add color to colors list that gets randomly chosen from in the generator feature.
+let generatorColorInput = document.getElementById('generator-color-input')
+let generatorAddColorButton = document.getElementById('generator-add-color-button')
+let generatorColorsList = document.getElementById('generator-added-colors')
+
+generatorAddColorButton.addEventListener('click', () => {
+    let color = generatorColorInput.value
+
+    let span = document.createElement('span')
+    span.className = 'colors-list-span'
+    span.style.color = color
+    span.innerHTML = color
+    generatorColorsList.appendChild(span)
+
+}, false)
+
+// Parse the 'colors-list-span' elements to get current list of chosen colors,
+// and return array of hexadecimal color values.
+const getChosenColors = () => {
+    let hexColors = []
+    let colorSpans = document.getElementsByClassName('colors-list-span')
+    
+    for (let i = 0; i < colorSpans.length; i++) {
+        hexColors.push(colorSpans[i].innerHTML)
+    }
+
+    return hexColors
+}
